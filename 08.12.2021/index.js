abdd065527c3d9a5635155547d66e4cebe82132c
eemaharramli <91149686+eemaharramli@users.noreply.github.com>
2021-12-09 16:22:41 +0400

    Update index.js

--- 08.12.2021/index.js
+++ 08.12.2021/index.js
@@ -19,70 +19,62 @@ let objects = [
 let result = objects.filter((value => value.age < 30))
 
 // console.log(result)
-
-//filter, find, every, some
-
-const test = [2,6,1,5,3,4]
-
-
-//Method Find
-
-function MethodFind(array, findElement){
-    for (let i = 0; i < array.length - 1; i++){
-        if (array[i] === findElement){
-            return array[i]
-        }
-    }
-}
-
-// console.log(myFind([1,2,3,4,5], 3))
-
-//Method Some
-
-function MethodSome(array){
-    let flag = false
-    for (let arrayElement of array) {
-        arrayElement > 5 ? flag = false : flag = true
-    }
-    return flag
-}
-
-// console.log(MethodSome([1,2,3,4,5]))
-
-//Method every
-
-function MyEvery(array) {
-    for (let number of array) {
-        number += 2
-    }
-}
-
-console.log(MyEvery(test))
-
-//Method Filter
-
-const MyFilter = array => {
-    for (let number of array) {
-        if (number > 3){
-            console.log(number)
-        }
-    }
-}
-// MyFilter(array)
-
+//Filter
 //elem.length > 5
 
 const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 
-function MyFilterMethod(array){
-	let result = [];
-  for(let element of array){
-  if(element.length > 5){
-  	result.push(element)
-  	}
-  }
-  return result
+function MyFilter(array){
+  if(Array.isArray(array)){
+  	let result = [];
+    for(let element of array){
+      if(element.length > 5){
+        result.push(element)
+        }
+      }
+    return result
+  }else return 'input must be an array'
 }
-console.log(MyFilter(words))
+console.log(`Method filter output: ${MyFilter(words)}`)
+
+//Some
+
+function MySome(array){
+  if(array.constructor === Array){
+    for(let element of array){
+      if(element.length > 5) return true
+    }
+  }else return 'input must be an array'
+}
+console.log(`Method some output: ${MySome(words)}`)
+
+//Every
+numbers = [5,3,2,4,1,5]
+function MyEvery(array){
+  if(array instanceof Array){
+    let flag = false
+    for(let element of array){
+      if(element > 3){
+        flage = true
+      }
+    }
+    return flag
+  }else return 'input must be an array'
+}
+console.log(`Method every output: ${MyEvery(numbers)}`)
+
+//Find
+function MyFind(array, conditionedElement){
+  if(Array.isArray(array)){
+  	for(let element of array){
+      if(element === conditionedElement){
+        return element
+      } 
+    }
+  }else return 'first parameter must be an array'
+}
+
+console.log(`Method Find output: ${MyFind(numbers, 5)}`)
+
 
 
